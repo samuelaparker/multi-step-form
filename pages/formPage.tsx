bf5d0dced93c24bf7d2b455083a58e750c4a57fe
samuelaparker <samuel.a.parker00@gmail.com>
2022-04-06 18:34:58 -0400

    add form label

--- pages/formPage.tsx
+++ pages/formPage.tsx
@@ -2,11 +2,18 @@ import JotformEmbed from "react-jotform-embed";
 import styles from "../styles/formPage.module.css";
 
 export default function FormPage() {
+    function submitHandler() {
+        console.log("Form submitted");
+    }
 
     return (
         <div>
-            <JotformEmbed src="https://form.jotform.com/220937556809063" />
-            
+            <form onSubmit={submitHandler}>
+                <label className="visuallyhidden" htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Name" />
+                <button>Submit!</button>
+            </form>
+
         </div>
     )
 }
