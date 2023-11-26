1b8ea2cca200e2bb01e569d5c4873616ffcd93b7
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-26 18:23:25 -0500

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
-//deploy
+export default MyApp;
